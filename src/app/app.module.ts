f8ca85b19009b8018eeefb7ef79dc0e6d626c0b5
andycodev <andycodev@gmail.com>
2022-06-21 17:31:18 -0500

    adding project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,68 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  NbAccordionModule, NbActionsModule,
+  NbAlertModule,
+  NbAutocompleteModule, NbBaseCalendarModule, NbButtonGroupModule,
+  NbButtonModule,
+  NbCardModule, NbCheckboxModule, NbContextMenuModule, NbDialogModule, NbFormFieldModule,
+  NbIconModule,
+  NbInputModule, NbListModule, NbPopoverModule, NbRadioModule,
+  NbSelectModule, NbSpinnerModule,
+  NbTabsetModule, NbTagModule, NbTimepickerModule, NbToggleModule, NbTooltipModule, NbUserModule
+} from "@nebular/theme";
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { HomeComponent } from './home/home.component';
+import { NbSidebarModule, NbMenuModule, NbThemeModule, NbLayoutModule } from '@nebular/theme';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomeComponent],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbIconModule,
+    NbSelectModule,
+    NbCardModule,
+    NbTabsetModule,
+    NbButtonModule,
+    NbIconModule,
+    NbInputModule,
+    NbSelectModule,
+    NbAutocompleteModule,
+    NbTimepickerModule,
+    NbRadioModule,
+    NbCheckboxModule,
+    NbTooltipModule,
+    NbToggleModule,
+    NbDialogModule.forChild(),
+    NbAlertModule,
+    NbListModule,
+    NbAccordionModule,
+    NbActionsModule,
+    NbPopoverModule,
+    NbSpinnerModule,
+    NbContextMenuModule,
+    NbUserModule,
+    NbFormFieldModule,
+    NbButtonGroupModule,
+    NbBaseCalendarModule,
+    NbTagModule,
+    NbSidebarModule.forRoot(),
+    NbMenuModule.forRoot(),
+    /*  NbThemeModule.forRoot({ name: 'default' }), */
+    NbThemeModule.forRoot({
+      name: window.matchMedia('(prefers-color-scheme: dark)').matches
+        ? 'dark'
+        : 'default',
+    }),
+    NbLayoutModule,
+    NbEvaIconsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
